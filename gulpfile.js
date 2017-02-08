db33f93b2f136ca2977c5822af8a4fa23b26be8f
zzl81cn <zzl81cn@163.com>
2017-02-08 16:24:21 +0800

    增加file-include 提供拼接静态HTML功能。

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,37 @@
  */
 
 var gulp = require('gulp'),
+		autoprefixer = require('gulp-autoprefixer'),
+		browserSync = require('browser-sync').create(),
 		// less = require('gulp-less'),
 		// mincss = require('gulp-minify-css'),
 		notify = require('gulp-notify'),
 		plumber = require('gulp-plumber'),
-		// sass = require('gulp-sass'),
-		sass = require('gulp-ruby-sass'),
-		autoprefixer = require('gulp-autoprefixer'),
+		sass = require('gulp-sass'),
+		// sass = require('gulp-ruby-sass'),
 		jsmin = require('gulp-uglify'),
-		browserSync = require('browser-sync').create();
+		fileInclude = require('gulp-file-include');
 
 // staticPath
 var src = './src',
 	dest = './dist';
-/*// proxyStaticPath
+// proxyStaticPath
 var	staticSrc = ['./src'],
-	proxyURL = 'http://192.168.16.167:9998';*/
+	proxyURL = 'http://192.168.16.167:9998';
 
 
 // gulp.task('serve', ['lessTask','sassTask','jsminTask'], function(){
 gulp.task('serve', ['sassTask','jsminTask'], function(){
 	browserSync.init({
 		// 启动server后打开默认页面
-		open: true,
+		open: false,
 		// scrollToTop
 		// scrollProportionally: false,
 		//镜像模式点击，滚动和表单在任何设备上输入将被镜像到所有设备里
 		ghostMode: {
-	    clicks: false,
-	    forms: true,
-	    scroll: false
+			clicks: false,
+			forms: true,
+			scroll: false
 		},
 		// staticServerPath
 		server: "src",
@@ -50,16 +51,16 @@ gulp.task('serve', ['sassTask','jsminTask'], function(){
 		}*/
 	});
 	// gulp.watch("./less/*.less", ['lessTask']);
-	gulp.watch("./src/styles/sass/*.scss", ['sassTask']);
+	gulp.watch("./src/styles/sass/**/*.scss", ['sassTask']);
 	// gulp.watch("./js/*.js", ['jsminTask']);
 	// node-sass require
 	// gulp.watch("./css/*.css", ['autoPrefixer']);
 	gulp.watch(["./src/**/*.html","./src/**/*.js"]).on('change', browserSync.reload);
 });
 
-// gulp-ruby-sass
-gulp.task('sassTask', function(){
-	return sass('./src/styles/sass/*.scss')
+// gulp-ruby-sass, Used gulp-sass replace this plugins.
+/*gulp.task('sassTask', function(){
+	return sass('./src/styles/sass/!*.scss')
     .on('error', sass.logError)
 		.pipe(autoprefixer({
 					browsers: ['last 2 versions','Firefox <= 20'],
@@ -67,22 +68,23 @@ gulp.task('sassTask', function(){
 				}))			
     .pipe(gulp.dest('./src/styles'))
     .pipe(browserSync.stream());
-});
+});*/
 
 // gulp-sass
-/*gulp.task('sassTask', function(){
- return gulp.src('./sass/!*.scss')
- .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
- // compressed,expanded
- .pipe(sass({outputStyle:'expanded'}))
- .pipe(autoprefixer({
- browsers: ['last 2 versions','Firefox <= 20'],
- cascade: false
- }))			
- .pipe(gulp.dest('./css'))
- .pipe(browserSync.stream());
- });*/
+gulp.task('sassTask', function () {
+	return gulp.src('./src/styles/sass/*.scss')
+		.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+		// compressed,expanded
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'Firefox <= 20'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./src/styles'))
+		.pipe(browserSync.stream());
+});
 
+// less task
 gulp.task('lessTask', function(){
 	// multiple files change to array type (['','',...])
 	return gulp.src('less/*.less')
@@ -103,6 +105,19 @@ gulp.task('jsminTask', function(){
 	return gulp.src('./js/*.js')
 		.pipe(jsmin())
 		.pipe(gulp.dest('./js-min'));
+});
+
+gulp.task('spliceHTML', function () {
+	// 适配src/pages中所有文件夹下的所有html，排除pages的include文件夹中html
+	gulp.src(['./src/build_page/*.html'])
+		.pipe(fileInclude({
+			prefix: '@@',
+			basepath: '@file',
+			context: {
+				name: 'Text parameter'
+			}
+		}))
+		.pipe(gulp.dest('./src'))
 });
 
 gulp.task('default', ['serve']);
